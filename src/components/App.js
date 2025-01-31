9b04cf9907785ce171b272aecbb31281ec433bfa
Rudranil8 <95670098+Rudranil8@users.noreply.github.com>
2025-01-31 12:23:44 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -13,7 +13,6 @@
 // export default App
 
 
-
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import { useParams } from "react-router-dom";
